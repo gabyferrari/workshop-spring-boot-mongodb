93b633bbb2fddb0ea1928b40745fc7facd9e6cd2
Gabrielly Ferrari <gaby.ferrari@outlook.com.br>
2024-05-28 17:18:30 -0300

    Operação de instanciação da base de dados

---
+++ src/main/java/com/gabyferrari/workshopmongo/config/Instantiation.java
@@ -0,0 +1,33 @@
+package com.gabyferrari.workshopmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.gabyferrari.workshopmongo.domain.User;
+import com.gabyferrari.workshopmongo.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+	//classe de teste, para instanciar a base de dados com dados que queremos
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		userRepository.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+	}
+
+}
