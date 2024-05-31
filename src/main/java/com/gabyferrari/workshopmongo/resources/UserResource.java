2e7bdc6f6a36a7c1697c40cac8b4edfb481cd56b
Gabrielly Ferrari <gaby.ferrari@outlook.com.br>
2024-05-31 17:05:43 -0300

    Deleção de usuário com DELETE

--- src/main/java/com/gabyferrari/workshopmongo/resources/UserResource.java
+++ src/main/java/com/gabyferrari/workshopmongo/resources/UserResource.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -44,5 +45,11 @@ public class UserResource {
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri(); //pega o endereço do novo obj q inserir
 		return ResponseEntity.created(uri).build(); //vai retornar uma resposta vazia com o codigo 201 e com o cabeçalho contendo a localizaçao do novo recurso criado
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable String id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
--- src/main/java/com/gabyferrari/workshopmongo/services/UserService.java
+++ src/main/java/com/gabyferrari/workshopmongo/services/UserService.java
@@ -30,6 +30,11 @@ public class UserService {
 		return repo.insert(obj);
 	}
 	
+	public void delete(String id) {
+		findById(id); //caso nao encontre o id, ja dar exceçao
+		repo.deleteById(id);
+	}
+	
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
